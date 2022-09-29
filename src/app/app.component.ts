aaee72dd775fafb8d70bbf0ecea1e4bb477a49bd
PC <fernandezfrancoa2001@gmail.com>
2022-09-28 23:27:53 -0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  
-  nombre: string = "Franco";
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
 
-  mostrarNombre(){
-    console.log(this.nombre)
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,11 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NoComunesComponent {
+  nombre: string = 'Franco';
+  genero: string = 'masculino';
 
-  
+  invitacionM = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
 
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Mario', 'Hernando'];
+  clientesM = {
+    '=0': 'no hay clientes.',
+    '=1': 'hay un cliente esperando',
+    '=2': 'hay 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  cambiarC() {
+    this.nombre = 'Denise';
+    this.genero = 'femenino';
+  }
+
+  borrarC() {
+    this.clientes.shift();
+  }
+
+  persona = {
+    nombre: 'Franco',
+    edad: 20,
+    dirección: 'Buenos Aires, Argentina',
+  };
+
+  heroes = [
+    { nombre: 'Batman', vuela: false },
+    { nombre: 'Superman', vuela: true },
+    { nombre: 'Wonder Woman', vuela: true }
+];
 }
